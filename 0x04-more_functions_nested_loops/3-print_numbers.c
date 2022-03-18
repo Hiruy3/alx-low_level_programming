a7b25fed9d99556b27bb53f5ef1bdd727a101d23
Hiruy3 <hiruytesfaye321@gmail.com>
2022-03-18 14:08:31 -0700

    taskks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,16 +10,4 @@ void print_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	_putchar(c);
 	_putchar('\n');
-}#include "holberton.h"
-
-/**
-* print_numbers - function that prints the numbers, from 0 to 9.
-*/
-void print_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	_putchar(c);
-	_putchar('\n');
 }
