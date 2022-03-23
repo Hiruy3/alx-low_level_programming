f4a7e331c256bc4d81e11296f7b44280c83785e1
Hiruy3 <hiruytesfaye321@gmail.com>
2022-03-23 12:28:17 -0700

    task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 #endif 
