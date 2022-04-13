c7a51e39881e674ca483490edfd99eb5e8374dc8
Hiruy3 <hiruytesfaye321@gmail.com>
2022-04-13 11:57:43 -0700

    task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS
-#define _FUNCTION_POINTERS
+#ifndef function_pointers
+#define function_pointers
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
