01972f74b8d4fa76f82438746db5b346b23509e6
Hiruy3 <hiruytesfaye321@gmail.com>
2022-06-17 13:01:41 -0700

    task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef main
-#define main
-
-/*copy list from task*/
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
